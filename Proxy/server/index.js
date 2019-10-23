3f1bfc5c5f3937bfbb4f9ad70e23c5d926ec539e
Jimmy Ma <jimmyma@Jimmys-MacBook-Pro.local>
2019-10-23 16:24:14 -0700

    proxy server working

---
+++ Proxy/server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const port = 3000;
+
+app.use('/air6n6/*/listing', express.static(path.join(__dirname, '../public')));
+
+// app.get('/now', (req, res) => {
+//   res.redirect('localhost:3010/now');
+// });
+
+app.listen(port, () => {
+  console.log(`App listening at ${port}`);
+});
+
